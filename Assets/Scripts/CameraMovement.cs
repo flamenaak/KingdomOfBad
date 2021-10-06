a91649c9128627d65d72bf42a9edf6d2f0b2f07a
Jakub_Platzek <jakub.platzek@gmail.com>
2021-10-06 17:15:41 +0200

    Movement basics

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundXWalk = 5f;
+    public float boundXSprint = 3f;
+    public bool isSprinting;
+    public float boundY = 0.05f;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        float boundX = isSprinting ? boundXSprint : boundXWalk;
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+
+
+
+}
--- Assets/Scripts/charMove.cs
+++ Assets/Scripts/charMove.cs
@@ -6,9 +6,13 @@ using System;
 public class charMove : MonoBehaviour
 {
     public float speed = 5f;
-    public float maxSpeed = 5f;
-    public float acceleration = 0.2f;
+    public float maxWalkSpeed = 5f;
+    public float maxSprintSpeed = 10f;
+    public float acceleration = 0.3f;
     private Rigidbody2D rigidBody;
+    private CameraMovement camera;
+
+    private float oldInput = 0f;
 
     private Animator animation;
     // Start is called before the first frame update
@@ -16,6 +20,7 @@ public class charMove : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody2D>();
         animation = GetComponent<Animator>();
+        camera = (CameraMovement)GameObject.FindGameObjectWithTag("MainCamera").GetComponent("CameraMovement");
     }
 
     // Update is called once per frame
@@ -24,13 +29,16 @@ public class charMove : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float newSpeed = GetNewSpeed(moveX, rigidBody.velocity.x, this.acceleration);
         rigidBody.velocity = new Vector2(newSpeed, rigidBody.velocity.y);
-
-        animation.SetFloat("speed", Math.Abs(newSpeed));
+        if (newSpeed != 0)
+            Debug.Log("newSpeed " + newSpeed + " input " + moveX);
+        //animation.SetFloat("speed", Math.Abs(newSpeed));
         // flip sprite if necessary
         if ((newSpeed < -0.1f && transform.localScale.x > 0)
         || (newSpeed > 0.1f && transform.localScale.x < 0))
+            
         {
             transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+           
         }
 
     }
@@ -39,12 +47,27 @@ public class charMove : MonoBehaviour
     {
         float newSpeed = velocity + (acceleration * input);
         int direction = GetDirection(newSpeed);
-        newSpeed = Math.Min(Math.Abs(newSpeed), maxSpeed);
+        newSpeed = Math.Min(Math.Abs(newSpeed), Input.GetKey("left shift") ? maxSprintSpeed : maxWalkSpeed);
+        if (isStopping(input) && Math.Abs(newSpeed) <= maxWalkSpeed)
+        {
+            newSpeed = 0;
+            camera.isSprinting = false;
+        }
+        else
+        {
+            camera.isSprinting = true;
+        }
+        oldInput = input;
         return newSpeed * direction;
     }
 
     int GetDirection(float speed)
     {
         return (speed > 0) ? 1 : -1;
+    }
+
+    bool isStopping(float input)
+    {
+        return Math.Abs(input) < Math.Abs(oldInput) || input == 0;
     }
 }
