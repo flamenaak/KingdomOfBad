d812b01dd8e14d2c074ee7c940bcffa5d8b189f6
Jakub_Platzek <jakub.platzek@gmail.com>
2022-02-02 22:09:36 +0100

    Implementation of basic and specific behaviour

---
+++ Assets/Bolt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public GameObject entity;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        /*if (entity.tag.Equals("Player"))
+        {
+            collision.GetComponentInParent<Enemy>().SendMessage("Damage", 1f);
+        }*/
+        Destroy(this);
+    }
+
+}
---
+++ Assets/Scripts/Enemy/Crossbowman/CrossbowmanAI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossbowmanAI : EnemyAI
+{
+    protected Crossbowman crossbowman;
+
+    public override void Awake()
+    {
+        base.Awake();
+        crossbowman = GetComponentInParent<Crossbowman>();
+        if (enemy == null)
+            Debug.LogError("Crossbowman AI awake cannot find crossbowman");
+    }
+
+    public override Transform DetectHostile()
+    {
+        return base.DetectHostile();
+    }
+
+    public override Vector2 DetermineDodgePosition(Vector2 target)
+    {
+        return Vector2.zero;
+        
+    }
+
+    public override Transform SearchForHostile()
+    {
+        return base.SearchForHostile();
+    }
+
+    public override bool ShouldChase(Transform entity)
+    {
+        if (!entity)
+            return false;
+        return Distance(entity) > 9;
+    }
+
+    public override bool ShouldDodge(Transform entity)
+    {
+        if (!entity)
+            return false;
+
+        float dist = Distance(entity);
+        return (dist < 5f);
+    }
+
+    public override bool ShouldMelleeAttack(Transform entity)
+    {
+        return base.ShouldMelleeAttack(entity);
+    }
+
+    public override bool ShouldRangeAttack(Transform entity)
+    {
+        float dist = Distance(entity);
+        return (dist >= 7.5f);
+    }
+
+}
